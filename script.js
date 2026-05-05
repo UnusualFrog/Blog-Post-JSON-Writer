1fb280eff777dbcf5fc08b69f6eb31b29384cf65
UnusualFrog <forwardnoah@gmail.com>
2026-05-05 19:28:43 -0230

    Added logic to force tags to lowercase

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const JSON_PATH = "./blog_posts.json";
+const JSON_PATH = "../blog_posts.json";
 
 const emojiPicker = document.querySelector('emoji-picker');
 const emojiButton = document.getElementById("emojiButton");
@@ -24,6 +24,7 @@ function addTag() {
   tagText += tagTextElem.value.trim();
   tagEmoji += emojiButton.textContent;
   tagFull = tagText + tagEmoji;
+  tagFull = tagFull.toLowerCase()
   // console.log(tag);
 
   // Check if tag exists
